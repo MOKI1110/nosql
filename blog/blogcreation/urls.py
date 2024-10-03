f0677eaa14aefa8348767cc287638e3f1b0fec47
MOKI1110 <118149856+MOKI1110@users.noreply.github.com>
2024-10-03 09:34:25 +0000

    Updated

---
+++ blog/blogcreation/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('base/',views.base, name="base"),
+    path('bloglist/',views.bloglist,name="blog"),
+    path('create_blog/',views.create_blog,name="blogcreate"),
+]
